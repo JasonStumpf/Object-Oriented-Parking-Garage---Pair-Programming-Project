d1d3794e07ed876d53fc898ad77a2035f81e5306
Jason Stumpf <jstumpf518@yahoo.com>
2023-05-27 22:47:47 -0700

    Added the Def

--- parking_garage.py
+++ parking_garage.py
@@ -1,11 +1,9 @@
 class Parking_garage ():
-    tickets = []            
-    parkingSpaces = []      # These are constant attributes
-    currentTicket = {}
-
-    def __init__(self):
-        pass
-        # Put necessary variables here
+    
+    def __init__(self, number_of_tickets, number_of_parking_space):
+        self.tickets = []            
+        self.parkingSpaces = []      
+        self.currentTicket = {}
 
     def takeTicket (self):
         pass
@@ -25,3 +23,24 @@ class Parking_garage ():
         # Once paid, display message "Thank you, have a nice day!"
         # Update parkingSpaces list to increase by 1 (meaning add to the parkingSpaces list)
         # Update tickets list to increase by 1 (meaning add to the tickets list)
+
+lot = Parking_garage(number_of_tickets = 10, number_of_parking_space = 10) #This calls the code from the class, for simplicity sake, the lot size is set to 10 spaces. Can increase number later.
+
+# This will run the program by using the lot to call the class functions
+def run_parking_garage():
+    print("\nWELCOME TO PARKING GARAGE")
+    while True:
+        response = input("\nWhat would you like to do? (Park/Pay/Leave/Quit): ")
+        if response.lower() == 'park':
+            pass
+        elif response.lower() == 'pay':
+            pass
+        elif response.lower() == 'leave':
+            pass
+        elif response.lower() == 'quit':
+            break
+        else:
+            print("That is not a valid response.")
+            pass
+
+run_parking_garage()
